2a8e3f9713d7bba319a3cdf78ebf3ce84bd85ed4
RandallWyatt <randallw16@fullsail.edu>
2014-10-14 19:03:07 -0400

    Expressions_Wacky
    Something Wacky calculated

---
+++ Declaring Variables/js/scriptVariable.js
@@ -0,0 +1,10 @@
+/**
+ * Created by rwyatt on 10/8/14.
+ */
+
+
+var age; //declaration
+age = 25; //definition
+//storage container = value
+
+var yearOfBirth = 1989; //declaring and defining on the same line
---
+++ Expressions_Wacky/js/Expressions_Wacky.js
@@ -0,0 +1,8 @@
+/**
+ * Created by rwyatt on 10/12/14.
+ */
+
+
+//Let's get a little crazy with this one
+
+//
