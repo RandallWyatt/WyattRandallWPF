3b391a100798d96940e395144af6ec0aa59e8173
RandallWyatt <randallw16@fullsail.edu>
2014-10-15 18:12:59 -0400

    oldEnough
    code seeing if the kid is old enough to ride

---
+++ ScreenCast Week 3/js/screenCast.js
@@ -0,0 +1,13 @@
+/**
+ * Created by rwyatt on 10/15/14.
+ */
+//Intro to Conditionals Screencast
+
+var oldEnough = false;
+
+//if the child is old enough print to the console you can ride
+if(oldEnough){
+    //code performed if condition is true
+    console.log("You can ride the coaster!")
+}
+console.log("What comes after")
