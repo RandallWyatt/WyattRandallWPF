4885af2aa5f547ef2a419d2b70793d8db1845ef0
RandallWyatt <randallw16@fullsail.edu>
2014-10-15 18:19:49 -0400

    sneakerLift
    conditionals with expression

--- ScreenCast Week 3/js/screenCast.js
+++ ScreenCast Week 3/js/screenCast.js
@@ -3,12 +3,13 @@
  */
 //Intro to Conditionals Screencast
 
-var kidHeight = 50;
+var kidHeight = 47;
 var minHeight = 48
+var sneakerLift = 2
 
 //if the child is old enough print to the console you can ride
 //if the kid is over 48 inches in height
-if(kidHeight > minHeight){
+if(kidHeight + sneakerLift > minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster!")
 }
