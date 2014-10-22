66022005efe68764661957750d7a39d104575519
RandallWyatt <randallw16@fullsail.edu>
2014-10-22 19:33:40 -0400

    arguments
    area calculation

--- FUNCTIONS intro/js/Functions_Intro.js
+++ FUNCTIONS intro/js/Functions_Intro.js
@@ -2,19 +2,16 @@
  * Created by rwyatt on 10/22/14.
  */
 
-function outptMsg(){
-    console.log("Hello World");
-}
+//function outptMsg(){
+    //console.log("Hello World");
 
-//Functions - Variable Scope
 
-var width = 5;
+//Functions - Parameters and Arguments
 
-function calcArea(){
-    var width = 20;
-    var height = 30;
-    var area = width * height;
+calcArea(30, 20);
+
+function calcArea(w, h){
+
+    var area = w * h;
     console.log(area);
 }
-calcArea();
-console.log(width);
