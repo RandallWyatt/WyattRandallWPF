4f78eaf0ffea2cd9bf64f449445eaaee48127cd4
RandallWyatt <randallw16@fullsail.edu>
2014-10-22 19:26:48 -0400

    Variable Scope
    Defining variable scope

--- FUNCTIONS intro/js/Functions_Intro.js
+++ FUNCTIONS intro/js/Functions_Intro.js
@@ -6,9 +6,15 @@ function outptMsg(){
     console.log("Hello World");
 }
 
+//Functions - Variable Scope
+
+var width = 5;
+
 function calcArea(){
     var width = 20;
     var height = 30;
     var area = width * height;
     console.log(area);
 }
+calcArea();
+console.log(width);
