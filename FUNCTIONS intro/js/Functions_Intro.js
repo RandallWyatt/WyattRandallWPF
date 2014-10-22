bbfaa2991ebfcb021d871c85ceb5c81b023a42d7
RandallWyatt <randallw16@fullsail.edu>
2014-10-22 19:43:45 -0400

    Parameters and arguments
    doge. Much wow.

--- FUNCTIONS intro/js/Functions_Intro.js
+++ FUNCTIONS intro/js/Functions_Intro.js
@@ -7,11 +7,18 @@
 
 
 //Functions - Parameters and Arguments
+//calcArea(30, 20);
 
-calcArea(30, 20);
+//function calcArea(w, h){
 
-function calcArea(w, h){
+    //var area = w * h;
+   // console.log(area);
 
-    var area = w * h;
-    console.log(area);
+
+function dogYears(age) {
+    var dogYears = age * 7;
+    console.log("Sparky is " + dogYears + " years old.");
 }
+var age1 = 4
+dogYears(age1);
+dogYears(5)
