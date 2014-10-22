c142f9143156bb45d0ebeb94d7947dc98be5a92e
RandallWyatt <randallw16@fullsail.edu>
2014-10-22 19:20:49 -0400

    Functions Intro
    first few lines of functions

---
+++ FUNCTIONS intro/js/Functions_Intro.js
@@ -0,0 +1,14 @@
+/**
+ * Created by rwyatt on 10/22/14.
+ */
+
+function outptMsg(){
+    console.log("Hello World");
+}
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
