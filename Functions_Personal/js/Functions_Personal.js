cb79a7d20da3453cccc2da29f435bb29be782678
RandallWyatt <randallw16@fullsail.edu>
2014-10-23 19:22:29 -0400

    Functions_Personal
    food costs

---
+++ Functions_Personal/js/Functions_Personal.js
@@ -0,0 +1,27 @@
+/**
+ * Created by rwyatt on 10/23/14.
+ */
+
+//Calculate food costs for a 4 week month
+
+calcFoodCosts(30, 20, 45, 30);
+
+function calcFoodCosts (w, h, m, e) {
+
+    var foodCosts = w + h + m + e;
+
+    console.log(foodCosts); prompt("YAY FOOD");
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
